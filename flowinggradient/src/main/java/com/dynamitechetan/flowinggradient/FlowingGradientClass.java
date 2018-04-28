6ee7019c73aae9fd77271b454c512609866d3fbf
Joran Dob <joran998@gmail.com>
2018-04-28 17:20:15 +0200

    Added constraintlayout implementation

--- flowinggradient/src/main/java/com/dynamitechetan/flowinggradient/FlowingGradientClass.java
+++ flowinggradient/src/main/java/com/dynamitechetan/flowinggradient/FlowingGradientClass.java
@@ -1,78 +1,86 @@
 package com.dynamitechetan.flowinggradient;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-
 
 /**
  * Created by DynamiteChetan on 17-07-2016.
  */
 public class FlowingGradientClass {
 
-    int Duration=4000;
-    RelativeLayout rl;
-    LinearLayout ll;
-    ImageView im;
-    int alphaint;
-    int d;
-    AnimationDrawable frameAnimation;
+  int Duration = 4000;
+  RelativeLayout relativeLayout;
+  LinearLayout linearLayout;
+  ImageView imageView;
+  ConstraintLayout constraintLayout;
+  int alphaint;
+  int resourceId;
+  AnimationDrawable frameAnimation;
 
-    public FlowingGradientClass setTransitionDuration(int time) {
-        this.Duration = time;
+  public FlowingGradientClass setTransitionDuration(int time) {
+    this.Duration = time;
 
-        return this;
+    return this;
+  }
+
+  public FlowingGradientClass onLinearLayout(LinearLayout ll) {
+    this.linearLayout = ll;
+    return this;
+  }
+
+  public FlowingGradientClass onImageView(ImageView im) {
+    this.imageView = im;
+    return this;
+  }
+
+  public FlowingGradientClass onRelativeLayout(RelativeLayout rl) {
+    this.relativeLayout = rl;
+    return this;
+  }
+
+  public FlowingGradientClass onConstraintsLayout(ConstraintLayout constraintLayout) {
+    this.constraintLayout = constraintLayout;
+    return this;
+  }
+
+  public FlowingGradientClass start() {
+
+    if (linearLayout != null) {
+      linearLayout.setBackgroundResource(resourceId);
+    } else if (relativeLayout != null) {
+      relativeLayout.setBackgroundResource(resourceId);
+    } else if (imageView != null) {
+      imageView.setBackgroundResource(resourceId);
+    } else if (constraintLayout != null) {
+      constraintLayout.setBackgroundResource(resourceId);
     }
-    public FlowingGradientClass onLinearLayout(LinearLayout ll) {
-        this.ll = ll;
-        return this;
+    if (linearLayout != null) {
+      frameAnimation = (AnimationDrawable) linearLayout.getBackground();
+    } else if (relativeLayout != null) {
+      frameAnimation = (AnimationDrawable) relativeLayout.getBackground();
+    } else if (imageView != null) {
+      frameAnimation = (AnimationDrawable) imageView.getBackground();
+    } else if (constraintLayout != null) {
+      frameAnimation = (AnimationDrawable) constraintLayout.getBackground();
     }
+    frameAnimation.setEnterFadeDuration(Duration);
+    frameAnimation.setExitFadeDuration(Duration);
+    frameAnimation.start();
 
-    public FlowingGradientClass onImageView(ImageView im) {
-        this.im = im;
-        return this;
-    }
+    return this;
+  }
 
-    public FlowingGradientClass onRelativeLayout(RelativeLayout rl) {
-        this.rl = rl;
-        return this;
-    }
-    public FlowingGradientClass start() {
+  public FlowingGradientClass setBackgroundResource(int resourceId) {
+    this.resourceId = resourceId;
+    return this;
+  }
 
-        if(ll!=null){
-            ll.setBackgroundResource(d);
-        }
-        else if(rl!=null){
-            rl.setBackgroundResource(d);
-        }
-        else if(im!=null){
-            im.setBackgroundResource(d);
-        }
-        if(ll!=null){
-            frameAnimation = (AnimationDrawable) ll.getBackground();
-        }
-        else if(rl!=null){
-            frameAnimation = (AnimationDrawable) rl.getBackground();
-        }
-        else if(im!=null){
-            frameAnimation = (AnimationDrawable) im.getBackground();
-        }
-        frameAnimation.setEnterFadeDuration(Duration);
-        frameAnimation.setExitFadeDuration(Duration);
-        frameAnimation.start();
-
-        return this;
-    }
-
-    public FlowingGradientClass setBackgroundResource(int d) {
-        this.d = d;
-        return this;
-    }
-    public FlowingGradientClass setAlpha(int alphaint) {
-        this.alphaint = alphaint;
-        frameAnimation.setAlpha(alphaint);
-        return this;
-    }
-
+  public FlowingGradientClass setAlpha(int alphaint) {
+    this.alphaint = alphaint;
+    frameAnimation.setAlpha(alphaint);
+    return this;
+  }
 }
